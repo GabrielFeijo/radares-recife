d86fcb931b589ccab9a735580eadc2b9624d6c87
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2026-08-23 18:42:04 -0300

    feat(map): lock default map tile to google maps with live traffic

--- src/constants/map.ts
+++ src/constants/map.ts
@@ -6,6 +6,12 @@ export const MAP_DEFAULTS = {
 	flyDuration: 1.5,
 } as const;
 
+export const MAP_TILES = {
+	GOOGLE_MAPS: {
+		url: "https://www.google.cn/maps/vt?lyrs=m@221097413,traffic&x={x}&y={y}&z={z}",
+	},
+} as const;
+
 export const CKAN_RESOURCE_IDS = {
 	RADARS: "36c2b47b-f439-4895-8b65-3f3dda36a4a7",
 	CAMERAS: "3d9a7f0d-cb38-48ee-9e10-d9b83284ae28",
