cad0a3d1f9f347e8fe85dd506592e7dcccceb7c7
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2026-08-23 18:50:25 -0300

    feat(types): expand PhotonProperties with osm tags and locality

--- src/types/photon.ts
+++ src/types/photon.ts
@@ -1,11 +1,17 @@
 export interface PhotonProperties {
 	osm_id?: number;
+	osm_key?: string;
+	osm_value?: string;
+	type?: string;
 	name?: string;
 	street?: string;
 	housenumber?: string;
-	city?: string;
 	district?: string;
+	locality?: string;
+	city?: string;
 	state?: string;
+	country?: string;
+	postcode?: string;
 }
 
 export interface PhotonFeature {
